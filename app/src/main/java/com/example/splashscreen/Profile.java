0de53079cd80aac195cad1bb6233e4f405322703
Gerdi <gerdiabdisalam@gmail.com>
2019-05-05 21:55:45 +0700

    update

--- app/src/main/java/com/example/splashscreen/Profile.java
+++ app/src/main/java/com/example/splashscreen/Profile.java
@@ -33,6 +33,15 @@ public class Profile extends Fragment {
                 startActivity(i);
             }
         });
+        ImageButton btn_map = (ImageButton) view.findViewById(R.id.btn_map);
+        btn_map.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse("https://www.google.com/maps/place/Jl.+Sukarajin,+Gandasari,+Katapang,+Bandung,+Jawa+Barat+40921/@-7.01993,107.5484564,17z/data=!3m1!4b1!4m5!3m4!1s0x2e68ec226846e547:0x97f054d1f3cc7b2c!8m2!3d-7.0199353!4d107.5506451"));
+                startActivity(i);
+            }
+        });
         return view;
     }
 }
